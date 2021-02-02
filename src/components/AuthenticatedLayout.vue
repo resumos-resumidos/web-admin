bd0639127bd0df7310333688fe2f6df14483e035
eduardolagolima <eduardolagolima@gmail.com>
2021-02-02 17:19:40 -0300

    feat: [CI/6] - cria layout padrão para as páginas da área logada

---
+++ src/components/AuthenticatedLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <SideMenu />
+    <v-main>
+      <v-container fluid>
+        <v-row
+          no-gutters
+          justify="center"
+        >
+          <v-col cols="12">
+            <slot />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SideMenu from './SideMenu.vue';
+
+export default {
+  name: 'AuthenticatedLayout',
+  components: {
+    SideMenu,
+  },
+};
+</script>
