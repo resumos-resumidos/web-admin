aeb9b161661a3bdb66211241909189667967b168
eduardolagolima <eduardolagolima@gmail.com>
2021-02-12 21:12:31 -0300

    feat: [CI/6] - cria a listagem das disciplinas com os botões de ação

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,9 @@ const api = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
 
+api.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error.response.data),
+);
+
 export default api;
--- src/views/Disciplines.vue
+++ src/views/Disciplines.vue
@@ -1,9 +1,46 @@
 <template>
-  <h1>Disciplines</h1>
+  <AuthenticatedLayout>
+    <h2 class="mb-3">
+      Disciplinas
+    </h2>
+    <CrudTable
+      :headers="headers"
+      :message-when-no-records="messageWhenNoRecords"
+      :rows="rows"
+    />
+  </AuthenticatedLayout>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
+import AuthenticatedLayout from '../components/Layouts/AuthenticatedLayout.vue';
+import CrudTable from '../components/Tables/CrudTable.vue';
+
+import api from '../services/api';
+
 export default {
   name: 'Disciplines',
+  components: {
+    AuthenticatedLayout,
+    CrudTable,
+  },
+  data: () => ({
+    headers: ['Título'],
+    messageWhenNoRecords: 'Não existe nenhuma disciplina cadastrada',
+    rows: [],
+  }),
+  async created() {
+    try {
+      const disciplines = await api.get('/disciplines');
+
+      disciplines.forEach(({ title }) => this.rows.push([title]));
+    } catch (error) {
+      this.SHOW_SNACKBAR({ show: true, text: error });
+    }
+  },
+  methods: {
+    ...mapMutations(['SHOW_SNACKBAR']),
+  },
 };
 </script>
