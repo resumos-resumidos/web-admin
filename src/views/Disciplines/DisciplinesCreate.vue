baaca6fa257cb1fb8725a6ec185bb54e5b317e7e
eduardolagolima <eduardolagolima@gmail.com>
2021-02-15 18:10:16 -0300

    feat: [CI/6] - adiciona funcionalidade de criação das disciplinas

--- src/views/Disciplines/DisciplinesCreate.vue
+++ src/views/Disciplines/DisciplinesCreate.vue
@@ -1,9 +1,50 @@
 <template>
-  <h1>DisciplinesCreate</h1>
+  <div>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="title"
+          label="Título"
+          :error-messages="errors.title"
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="primary"
+        @click="create"
+      >
+        Salvar
+      </v-btn>
+    </v-card-actions>
+  </div>
 </template>
 
 <script>
+import api from '../../services/api';
+
 export default {
   name: 'DisciplinesCreate',
+  data: () => ({
+    title: null,
+    errors: {},
+  }),
+  methods: {
+    async create() {
+      try {
+        this.errors = {};
+
+        const { title } = this;
+        await api.post('/disciplines', { title });
+
+        this.$router.push('/disciplines');
+      } catch (errors) {
+        Object.keys(errors).forEach((field) => {
+          this.errors = { [field]: errors[field].join(' / '), ...this.errors };
+        });
+      }
+    },
+  },
 };
 </script>
