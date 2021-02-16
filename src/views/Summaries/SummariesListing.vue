9c72856a2ef3aa261d6e52f46b832d7a959f6fd1
eduardolagolima <eduardolagolima@gmail.com>
2021-02-16 15:38:39 -0300

    feat: [CI/8] - cria a listagem dos resumos com os botões de ação

--- src/views/Summaries/SummariesListing.vue
+++ src/views/Summaries/SummariesListing.vue
@@ -1,9 +1,49 @@
 <template>
-  <h1>SummariesListing</h1>
+  <v-card-text>
+    <CrudTable
+      :table-headers="headers"
+      :message-when-no-records="messageWhenNoRecords"
+      :table-rows.sync="rows"
+    />
+  </v-card-text>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
+import CrudTable from '../../components/Tables/CrudTable.vue';
+
+import api from '../../services/api';
+
 export default {
   name: 'SummariesListing',
+  components: {
+    CrudTable,
+  },
+  data: () => ({
+    headers: ['Disciplina', 'Contéudo', 'Resumo', 'Gratuito?'],
+    messageWhenNoRecords: 'Não existe nenhum resumo cadastrado',
+    rows: [],
+  }),
+  async created() {
+    try {
+      const summaries = await api.get('/summaries');
+
+      summaries.forEach((summary) => this.rows.push({
+        id: summary.id,
+        columns: [
+          summary.content.discipline.title,
+          summary.content.title,
+          summary.title,
+          summary.free ? 'Sim' : 'Não',
+        ],
+      }));
+    } catch (error) {
+      this.HANDLE_SNACKBAR({ show: true, text: error });
+    }
+  },
+  methods: {
+    ...mapMutations(['HANDLE_SNACKBAR']),
+  },
 };
 </script>
