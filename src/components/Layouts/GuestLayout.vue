959031fa739191d6507d4019be2cb828b1f33844
eduardolagolima <eduardolagolima@gmail.com>
2021-03-09 22:44:35 -0300

    feat: [CI/11] - cria layout padrão para as páginas que não necessitam de autenticação

---
+++ src/components/Layouts/GuestLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout>
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="6"
+            lg="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar flat>
+                <v-toolbar-title>
+                  Resumos Resumidos
+                </v-toolbar-title>
+              </v-toolbar>
+              <slot />
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </Layout>
+</template>
+
+<script>
+import Layout from './Layout.vue';
+
+export default {
+  name: 'GuestLayout',
+  components: {
+    Layout,
+  },
+};
+</script>
