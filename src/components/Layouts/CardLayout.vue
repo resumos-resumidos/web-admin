3e90879763968f9639dbcc82a819be7f15b8a210
eduardolagolima <eduardolagolima@gmail.com>
2021-03-10 21:50:35 -0300

    feat: [CI/12] - cria componente do card

---
+++ src/components/Layouts/CardLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="elevation-12">
+    <v-toolbar flat>
+      <v-toolbar-title>
+        <slot name="v-toolbar-title" />
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <slot name="v-card-text" />
+    </v-card-text>
+    <v-card-actions>
+      <slot name="v-card-actions" />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CardLayout',
+};
+</script>
