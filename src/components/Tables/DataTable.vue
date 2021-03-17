b897b7759fafb22c75363c59af1ec5949a27199d
eduardolagolima <eduardolagolima@gmail.com>
2021-03-16 23:09:49 -0300

    feat: [CI/13] - cria componente DataTable

---
+++ src/components/Tables/DataTable.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="5"
+    :no-data-text="messageWhenNoRecords"
+  >
+    <template v-slot:[`header.actions`]>
+      <v-btn
+        icon
+        :to="`${routePath}/create`"
+      >
+        <v-icon>
+          mdi-plus-circle-outline
+        </v-icon>
+      </v-btn>
+    </template>
+    <template v-slot:[`item.actions`]="{ item }">
+      <v-btn
+        icon
+        :to="`${routePath}/edit/${item.id}`"
+      >
+        <v-icon>
+          mdi-pencil
+        </v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click="$emit('destroy', item.id)"
+      >
+        <v-icon>
+          mdi-delete
+        </v-icon>
+      </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    messageWhenNoRecords: {
+      type: String,
+      required: true,
+    },
+    routePath: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
