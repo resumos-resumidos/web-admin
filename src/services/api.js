4d4ce76feb433f5600746ed57a36df5618664e94
eduardolagolima <eduardolagolima@gmail.com>
2021-03-28 20:15:44 -0300

    feat: [CI/11] - ajusta para enviar o token de autenticação em todas as requisições

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,20 @@ const api = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    const newConfig = config;
+
+    if (accessToken) {
+      newConfig.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return newConfig;
+  },
+  (error) => Promise.reject(error),
+);
+
 api.interceptors.response.use(
   (response) => response.data,
   (error) => Promise.reject(error.response.data),
